b754a4b6a3428fa9202d8a0962eabee0bd9d6092
gunkiddedy <gunkiddedy@gmail.com>
2023-09-30 12:03:43 +0700

    set the custom media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,34 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'xs': '390px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      /**
+       * define MIN and MAX
+        'xs': {'min': '375px', 'max': '639px'}, //red
+        'sm': {'min': '640px', 'max': '767px'}, //green
+        'md': {'min': '768px', 'max': '1023px'}, //purple
+        'lg': {'min': '1024px', 'max': '1279px'}, //yellow
+        'xl': {'min': '1280px', 'max': '1535px'}, //white
+        '2xl': {'min': '1536px'}, //blue
+      */
+      
+      // define MIN
+      'xs': '375px', //red
+      'sm': '640px', //green
+      'md': '768px', //purple
+      'lg': '1024px', //yellow
+      'xl': '1280px', //white
+      '2xl': '1536px', //blue
     },
     extend: {},
   },
   plugins: [],
 }
+
+/**
+ *  320px — 480px: Mobile devices
+    481px — 768px: iPads, Tablets
+    769px — 1024px: Small screens, laptops
+    1025px — 1200px: Desktops, large screens
+    1201px and more —  Extra large screens, TV
+ */
 

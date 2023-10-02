6be81ca96aa98c089ac9d3c2ead134d325da2a04
gunkiddedy <gunkiddedy@gmail.com>
2023-10-02 14:39:42 +0700

    fix backdrop issue on mobile screens

--- assets/main.js
+++ assets/main.js
@@ -160,14 +160,14 @@ const sidenav = document.getElementById("sidenav")
 // open mobile menu
 function openNav() {
     document.body.style.overflow = 'hidden'
-    navbarBackdrop.classList.add('fixed', 'inset-0')
+    navbarBackdrop.classList.add('fixed', 'inset-0', 'h-[100vh]')
     sidenav.classList.remove('-top-[500px]')
     sidenav.classList.add('top-0') 
 }
 
 // close mobile menu
 function closeNav() {
-    navbarBackdrop.classList.remove('fixed', 'inset-0')
+    navbarBackdrop.classList.remove('fixed', 'inset-0', 'h-[100vh]')
     sidenav.classList.remove('top-0')
     sidenav.classList.add('-top-[500px]')
     document.body.style.overflow = ''
